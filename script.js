99fb251e983bcaa801a6f411d1d64496bcf096ea
Saeed212223 <ghulamsaeed54@gmail.com>
2023-12-06 11:51:37 +0430

    mount ability

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 const vm = Vue.createApp({
     data(){
        return{
-         perspective: 100 , 
-         rotateX: 0,
-         rotateY: 0 ,
-         rotateZ:0
+        message: 'Hello World'
        }
     },
-    computed: {
-      box(){
-         return{
-            transform: `perspective(${this.perspective}px)
-            rotateX(${this.rotateX}deg)
-            rotateY(${this.rotateY}deg)
-            rotateZ(${this.rotateZ}deg)`
-         }
-      }
-    } , 
-    methods:{
-      reset(){
-         this.perspective =  100 , 
-         this.rotateX =  0,
-         this.rotateY =  0 ,
-         this.rotateZ = 0
-      } , 
-      async copy(){
-         let text = `transform:${this.box.transform};`
-         await navigator.clipboard.writeText(text)
-         alert("CSS Copy to clipboard")
-
-      }
-    }
    
- }).mount('#app')
+ })
+ vm.mount('#app')
+
+//  setTimeout(() => {
+//    vm.mount('#app')
+//  }, 3000);
 
 
 
