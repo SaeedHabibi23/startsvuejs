61058ee2d88c889c7c4ce384759656a6915401f1
Saeed212223 <ghulamsaeed54@gmail.com>
2023-12-06 12:17:05 +0430

    Some Hooks lifecycle worked

--- script.js
+++ script.js
@@ -4,8 +4,33 @@ const vm = Vue.createApp({
         message: 'Hello World'
        }
     },
+    beforeCreated(){
+      console.log('beforeCreated() function is called' , this.message)
+    },
+    created(){
+      console.log('created() function is called' , this.message)
+    },
+    beforeMount(){
+      console.log('beforeMount() function is called' , this.$el)
+    },
+    mounted(){
+      console.log('mounted() function is called' , this.$el)
+    } ,
+    beforeUpdate(){
+      console.log('beforeUpdate() function is called')
+    },
+    updated(){
+      console.log('updated() function is called')
+    },
+    beforeUnmount(){
+      console.log('beforeUnmount() function is called')
+    },
+    unmounted(){
+      console.log('unmount() function is called')
+    },
    
  })
+
  vm.mount('#app')
 
 //  setTimeout(() => {
