e9f37b01f2120194260bad96bdc7d3816ffcaae5
Saeed212223 <ghulamsaeed54@gmail.com>
2023-12-06 11:38:26 +0430

    Copy to clipboard worked

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ const vm = Vue.createApp({
          this.rotateX =  0,
          this.rotateY =  0 ,
          this.rotateZ = 0
+      } , 
+      async copy(){
+         let text = `transform:${this.box.transform};`
+         await navigator.clipboard.writeText(text)
+         alert("CSS Copy to clipboard")
+
       }
     }
    
